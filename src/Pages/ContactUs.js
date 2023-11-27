f24eb960549469dfe0daaa5935db3024740a216c
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-11-27 11:06:27 +0530

    conact us page with firebase(t-12)

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -3,11 +3,48 @@ import Footer from "../Component/Layout/Footer/Footer";
 import CartProvider from "../Store/CartProvider";
 import HeaderCard from "../Component/UI/Card";
 import { Col, Form, Button, Container,Row } from "react-bootstrap";
-import contact from "../Assets/contact.jpg";
+import contactImg from "../Assets/contact.jpg";
+import { useState } from "react";
 
-function Contact() {
+function Contact(props) {
+  const [name,setName]=useState('')
+  const [contact,setContact]=useState('')
+  const [email,setEmail]=useState('')
 
- 
+const nameHandler=(event)=>{
+  const nameRef=event.target.value
+  setName(nameRef)
+  
+}
+
+const contactHandler=(event)=>{
+  const contactRef=event.target.value
+  setContact(contactRef)
+}
+
+const emailHandler=(event)=>{
+  const emailRef=event.target.value
+  setEmail(emailRef)
+}
+
+const onSubmitHandler =async(event)=>{
+  event.preventDefault()
+ const contactDetails={
+    name:name,
+    contact:contact,
+    email:email
+  }
+  console.log(contactDetails)
+  const response=await fetch('https://react-http-6e598-default-rtdb.firebaseio.com/contactDetails.json',{
+      method:'POST',
+      body:JSON.stringify(contactDetails),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    const data=await response.json()
+    console.log(data)
+}
 
 
   return (
@@ -16,7 +53,7 @@ function Contact() {
       <HeaderCard>        
         <Col md={5}>
           <img
-            src={contact}
+            src={contactImg}
             alt="contact png"
             className="px-3 py-2"
             style={{ width: "100%" }}
@@ -30,21 +67,21 @@ function Contact() {
           <h1>Contact us</h1>
         </Col>        
         <Col md={6} className="border rounded-3 border-primary p-3">
-          <Form className="my-2 py-1">
+          <Form className="my-2 py-1" onSubmit={onSubmitHandler}>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Name:</Form.Label>
-              <Form.Control type="text" placeholder="Enter Name"  />              
+              <Form.Control type="text" placeholder="Enter Name" onChange={nameHandler} />              
             </Form.Group> 
             <Form.Group className="mb-3" controlId="contact">
               <Form.Label>Contact No:</Form.Label>
-              <Form.Control type="number" placeholder="Enter Contact number" />  
+              <Form.Control type="number" placeholder="Enter Contact number" onChange={contactHandler} />  
               <Form.Text className="text-muted">
                 We'll never share your contact number with anyone else.
               </Form.Text>            
             </Form.Group> 
             <Form.Group className="mb-3" controlId="email">
               <Form.Label>Email address:</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control type="email" placeholder="Enter email" onChange={emailHandler}/>
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
